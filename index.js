48fd745cd80bf522a00725a9a9132d73bbf95170
VMORI0112 <victorfernan_moriganador@hotmail.com>
2019-09-27 21:56:54 +0000

    everything good

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
-
-
-
-//console.log(generate1());
-
-document.querySelector("#cardnumber1").innerHTML = generate1() ;
-
-document.querySelector("#cardnumber2").innerHTML = generate2();
-
 function generate1(){
-
-     return Math.floor(Math.random() * 9) + 1;
+     let card = Math.floor(Math.random() * 9) + 1;
+        document.querySelector("#cardnumber1").innerHTML = `${card}`;
 }
-function generate2(){
 
-     return Math.floor(Math.random() * 9) + 1;
+function generate2(){
+     let card2 =  Math.floor(Math.random() * 9) + 1;
+        document.querySelector("#cardnumber2").innerHTML = `${card2}`;
 }
